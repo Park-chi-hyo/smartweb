6299e0e4867a188cbf971dde8225b6b4fc14c97b
Administrator <Administrator@mars70-PC>
2019-08-13 09:36:13 +0900

    first commit test v1.0

---
+++ 0722/src/test/ResponseServlet.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Response")
+public class ResponseServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out=resp.getWriter();
+		out.println("Response요청성공!!");
+		out.println();
+		out.close();
+	}
+	
+}
